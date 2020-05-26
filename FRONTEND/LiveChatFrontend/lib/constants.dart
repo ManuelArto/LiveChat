ab566fe1e45bb7182102ff3fca41b2e08b8c2f16
ManuelArto <manuelartociao@gmail.com>
2020-05-26 09:08:20 +0200

    public url

--- FRONTEND/LiveChatFrontend/lib/constants.dart
+++ FRONTEND/LiveChatFrontend/lib/constants.dart
@@ -1,5 +1,5 @@
-const URL_AUTH_SIGNUP = "http://192.168.1.33:5000/api/register";
-const URL_AUTH_SIGNIN = "http://192.168.1.33:5000/api/login";
-const URL_AUTH_REFRESH_TOKEN = "http://192.168.1.33:5000/api/refresh-token";
+const URL_AUTH_SIGNUP = "https://live-chat-fp.herokuapp.com/api/register";
+const URL_AUTH_SIGNIN = "https://live-chat-fp.herokuapp.com/api/login";
+const URL_AUTH_REFRESH_TOKEN = "https://live-chat-fp.herokuapp.com/api/refresh-token";
 
-const URL_SOCKETIO = "http://192.168.1.33:5000";
+const URL_SOCKETIO = "https://live-chat-fp.herokuapp.com";
